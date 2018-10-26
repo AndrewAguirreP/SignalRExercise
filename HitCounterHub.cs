8cd1537f50be04e47ba75bdcf1704da336c2cec1
AndrewAguirreP <seesharpprojects@hotmail.com>
2018-10-26 12:00:15 +0800

    Add files via upload

---
+++ HitCounterHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRExcercise
+{
+    [HubName("hitCounter")]
+    public class HitCounterHub : Hub
+    {
+        static int _hitCount = 0;
+        public void RecordHit()
+        {
+            _hitCount += 1; 
+            Clients.All.onRecordHit(_hitCount); 
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _hitCount -= 1;
+            Clients.All.onRecordHit(_hitCount);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
